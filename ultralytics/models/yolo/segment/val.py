373e40252af86ac7f6810a611b33571cbd79b652
SaadNoor555 <48786753+SaadNoor555@users.noreply.github.com>
2023-08-13 12:51:36 +0600

    print only when table present

--- ultralytics/models/yolo/segment/val.py
+++ ultralytics/models/yolo/segment/val.py
@@ -185,7 +185,7 @@ class SegmentationValidator(DetectionValidator):
             #         print(type(pm))
             
             iou = mask_iou(gt_masks.view(gt_masks.shape[0], -1), pred_masks.view(pred_masks.shape[0], -1))
-            print(iou)
+            if tcnt!=0: print(iou)
         else:  # boxes
             iou = box_iou(labels[:, 1:], detections[:, :4])
 
